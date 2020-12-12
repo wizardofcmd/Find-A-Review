0ef6e3b6628dea599ecc231f19f7e41495c79cbe
Sorin Nechifor <sorinnechiforora@gmail.com>
2020-12-12 23:07:45 +0000

    Arbor implementation start

--- static/js/form.js
+++ static/js/form.js
@@ -1,3 +1,15 @@
+function ajax($this) {
+  var book_data = $this.attr('name');
+  console.log(book_data);
+  var sys = arbor.ParticleSystem(10, 400, 1);
+  sys.parameters({
+    gravity: false,
+    repulsion: 150
+  });
+  sys.renderer = Renderer("#viewport");
+
+
+}
 $(document).ready(function(){
     $( "#btn-search" ).click(function() {
         $.ajax({
@@ -7,9 +19,25 @@ $(document).ready(function(){
             url : '/process'
         })
         .done(function(data){
-            console.log(data)
+var data = JSON.stringify(data);
+
         });
 
         event.preventDefault();
     });
 });
+// Have a function that creates element instead to refresh canvas
+function loadcanvas(id) {
+  var canvas = document.createElement('canvas');
+  var div = document.getElementById(id)
+  canvas.id = "viewport";
+  canvas.width = "1108";
+  canvas.height = "688";
+  canvas.class = "model-content"
+  div.appendChild(canvas);
+}
+// Still don't know how to refresh so instead, delete the canvas
+function absolutelyDestroyCanvas(id) {
+  var elem = document.getElementById(id);
+  elem.parentNode.removeChild(elem);
+}
